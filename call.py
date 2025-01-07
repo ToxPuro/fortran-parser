26f654b082125e4be86d8d2dee2234af0808b4fc
Touko Puro <toukopur@puhti-login11.bullx>
2025-01-07 15:13:58 +0200

    also save call.py

--- call.py
+++ call.py
@@ -9,7 +9,7 @@ def main():
             #"bc_ss_temp_old",
             #"bc_ss_temp_x",
             #"bc_ss_temp_y",
-            "bc_ss_temp_z",
+            #"bc_ss_temp_z",
             #"bc_lnrho_temp_z",
             #"bc_lnrho_pressure_z",
             #"bc_ss_temp2_z",
@@ -24,10 +24,11 @@ def main():
             #"bc_lnrho_cfb_r_iso",
             #"bc_lnrho_hds_z_iso",
             #"bc_ism",
+            "rhs_cpu"
         ]
     for func in funcs:
-        command = f"python3 parse.py -f {func} -F ~/my_pc_copy/samples/gputest/src/eos_idealgas.f90 -o -b --sample-dir ~/my_pc_copy/samples/gputest --dir ~/my_pc_copy/samples/gputest/src "
-        os.system("rm -f res-inlined.txt")
+        command = f"python3 parse.py -f {func} -F ~/pc-transpile/pencil-code/samples/magnetic-decay/src/equ.f90 -o -s --sample-dir ~/pc-transpile/pencil-code/samples/magnetic-decay --dir ~/pc-transpile/pencil-code/samples/magnetic-decay/src "
+        #os.system("rm -f res-inlined.txt")
         os.system(command)
 
 if __name__ == "__main__":
